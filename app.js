a3d5a6efe3a21d49cab9443ff71102a91b3b0e02
Ana Goncalves <alsgoncalves91@gmail.com>
2020-09-28 22:37:52 +0100

    JS free trial message pop up

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
+// const submit = document.querySelector('input[type=submit]');
+
+
+// const messages = submit.addEventListener('click', () => {
+//     // const inputText = document.querySelectorAll('input[type=text]');
+//     const inputText = document.querySelector('input[type=text]');
+//     if (inputText === '') {
+//         const p = document.createElement('p');
+//         p.innerText = "Please Enter First Name";
+//         inputText.appendChild(img);
+//     }
+// });
+
+// messages();
+
+const inputText = document.querySelector('input[type=text]');
+const formDiv = document.querySelector('.form-container');
 const submit = document.querySelector('input[type=submit]');
 
+// const p = document.createElement('p');
+// p.innerText = "Please Enter First Name";
+// const add = inputText.appendChild(p);
 
-const messages = submit.addEventListener('click', () => {
-    // const inputText = document.querySelectorAll('input[type=text]');
-    const inputPass = document.querySelector('input[type=password]');
-    if (inputPass === '') {
-        const img = document.createElement('img');
-        img.src = 'alert.png'
-        inputPass.appendChild(img);
-    }
-    return img
+submit.addEventListener("click", function(event) {
+   event.preventDefault(); 
+   const blueAlert = document.createElement('div');
+   const p = document.createElement('p');
+   blueAlert.appendChild(p);
+   console.log(blueAlert);
+   p.innerHTML = '<b>Join for Free 30 days</b> then $59/mo. after';
+   blueAlert.classList.add('blue');
+    formDiv.insertAdjacentElement("beforebegin", blueAlert);
 });
-
